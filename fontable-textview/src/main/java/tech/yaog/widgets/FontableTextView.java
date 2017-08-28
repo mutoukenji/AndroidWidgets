dba53a2f427c04241620d10ad531cc73f1132b2f
yaoguanglin <yaoguanglin@eryu.group>
2017-08-28 16:33:24 +0800

    添加FontableTextView

---
+++ fontable-textview/src/main/java/tech/yaog/widgets/FontableTextView.java
@@ -0,0 +1,59 @@
+package tech.yaog.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import tech.yaog.widgets.fontabletextview.R;
+
+/**
+ * 可定义字体的TextView
+ * Created by mutoukenji on 17-8-28.
+ */
+
+public class FontableTextView extends android.support.v7.widget.AppCompatTextView {
+
+    private String font;
+
+    public FontableTextView(Context context) {
+        super(context);
+        init(context, null, 0, 0);
+    }
+
+    public FontableTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0, 0);
+    }
+
+    public FontableTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, 0);
+    }
+
+    private void init(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.FontableTextView, defStyleAttr, defStyleRes);
+
+        String fontPath = typedArray.getString(R.styleable.FontableTextView_custom_font);
+        if (fontPath != null) {
+            setFont(fontPath);
+        }
+
+        typedArray.recycle();
+    }
+
+    public void setFont(String customFont) {
+        if (customFont != null) {
+            if (!font.equals(customFont)) {
+                try {
+                    Typeface typeFace = Typeface.createFromAsset(this.getContext().getAssets(), customFont);
+                    setTypeface(typeFace);
+                    font = customFont;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
